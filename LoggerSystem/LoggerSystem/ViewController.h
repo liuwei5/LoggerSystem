9e38d22d14dee6f9a224b560e43de04d75d7d5e4
liuwei5 <jingzhouliuwei@126.com>
2015-09-06 10:11:01 +0800

    add LoggerSystem code

---
+++ LoggerSystem/LoggerSystem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoggerSystem
+//
+//  Created by BrotherWei on 15/9/6.
+//  Copyright (c) 2015年 paDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LoggerSystem/LoggerSystem/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LoggerSystem
+//
+//  Created by BrotherWei on 15/9/6.
+//  Copyright (c) 2015年 paDev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
